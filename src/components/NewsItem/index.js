9112481fcc9eee8d5fa5ce1c05a16ad10861dbc8
sasi-kr <sasi.webdev@gmail.com>
2023-12-11 23:30:28 +1100

    simplified

--- src/components/NewsItem/index.js
+++ src/components/NewsItem/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './newsItem.css';
 
-function NewsItem({ newsItem }) {
-  const { title, author, publishedAt, description, content, url, urlToImage } =
-    newsItem;
+function NewsItem({ urlToImage, url, title, description }) {
   return (
-    <div className="card">
+    <div className="news-app">
       <img src={urlToImage} alt={title} />
-
-      <h2>{title}</h2>
-      <p>
-        {description} - {author} Published at : {publishedAt}
-      </p>
-      <p>{content}</p>
+      <p>{description}</p>
       <a href={url} target="_blank" rel="noopener noreferrer">
-        More Info
+        <h3>{title}</h3>
       </a>
     </div>
   );
