cabcc18875d77068ff272cef01be20494c3e8986
sasi-kr <sasi.webdev@gmail.com>
2023-12-11 12:55:20 +1100

    first push to test ci-cd pipeline

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -7,14 +7,18 @@ function App() {
 
   const [loading, setLoading] = useState(true);
   const [newsData, setNewsData] = useState({});
+  const [searchInput, setSearchInput] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState('au');
 
-  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  // const url = `https://newsapi.org/v2/top-headlines?country=${selectedCountry}&apiKey=${API_KEY}`;
+  // const url = `https://newsapi.org/v2/everything?q=${searchInput}&from=2023-11-07&sortBy=popularity&apiKey=${API_KEY}`;
   const options = {
     method: 'GET',
     headers: { accept: 'application/json' },
   };
 
-  const fetchData = async () => {
+  async function fetchData(url) {
     setLoading(true);
     // setError(null);
     try {
@@ -30,10 +34,28 @@ function App() {
     } finally {
       setLoading(false);
     }
+  }
+
+  const handleSubmit = (e) => {
+    setSearchInput(e.target.value);
   };
+  // console.log(searchInput);
+
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+  // console.log(selectedCountry);
+
+  // useEffect(() => {
+  //   const url = `https://newsapi.org/v2/everything?q=${searchInput}&from=2023-11-07&sortBy=popularity&apiKey=${API_KEY}`;
+  //   fetchData(url);
+  // }, [searchInput]);
+
   useEffect(() => {
-    fetchData();
-  }, []);
+    const url = `https://newsapi.org/v2/top-headlines?country=${selectedCountry}&apiKey=${API_KEY}`;
+
+    fetchData(url);
+  }, [selectedCountry]);
   // console.log(newsData);
   return (
     <div className="bg-gray-100 min-h-screen">
@@ -41,7 +63,33 @@ function App() {
         {' '}
         <h1 className="text-3xl font-semibold underline">News App</h1>
       </header>
+
       <main className="container mx-auto py-6">
+        <div>
+          <label htmlFor="countrySelect">Select Country:</label>
+          <select
+            id="countrySelect"
+            value={selectedCountry}
+            onChange={handleCountryChange}
+          >
+            <option value="au">Australia</option>
+            <option value="us">United States</option>
+            <option value="jp">Japan</option>
+            <option value="sg">Singapore</option>
+            <option value="in">India</option>
+          </select>
+        </div>
+        <form action="" onSubmit={handleSubmit}>
+          <label htmlFor="searchInput">Search News</label>
+          <input
+            type="text"
+            value={searchInput}
+            name="searchInput"
+            className="ml-5 pl-2"
+          />
+          <button className="btn-sm">Submit</button>
+        </form>
+
         {loading ? (
           <p className="text-center">Loading...</p>
         ) : (
